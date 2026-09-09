efcc05ca03f9cd7ec7e714763b96fff694d635ee
Bandith <bandith2323@gmail.com>
2026-09-09 14:40:45 +0700

    Added rental and customer class and initilized data

--- Main.cpp
+++ Main.cpp
@@ -136,6 +136,118 @@ public:
         this->passengerCapacity = passengerCapacity;
     }
 };
+
+    class Rental{
+        private:
+        double totalPrice;
+        double securityDeposit;
+        string paymentStatus;
+
+        public:
+        Rental(double totalPrice, double securityDeposit, string paymentStatus) {
+            this-> totalPrice = totalPrice;
+            this -> securityDeposit = securityDeposit;
+            this-> paymentStatus = paymentStatus;
+        }
+
+        double gettotalprice() {
+            return totalPrice;
+        }
+
+        void settotalprice(double totalPrice) {
+            this-> totalPrice = totalPrice;
+        }
+
+        double getsecuritydeposit() {
+            return securityDeposit;
+        }
+        void setsecurityDeposit(double securityDeposit) {
+            this-> securityDeposit = securityDeposit;
+        }
+        string getpaymentstatus() {
+            return paymentStatus;
+        }
+        void setpaymentstatus(string paymentStatus) {
+            this-> paymentStatus = paymentStatus;
+    }
+    void displayInfo() {
+        cout << "Total Price: $" << totalPrice << endl;
+        cout << "Security Deposit: $" << securityDeposit << endl;
+        cout << "Payment Status: " << paymentStatus << endl;
+        
+    }
+
+};
+
+    class Customer{
+
+        private:
+        string name;
+        int customerId;
+        string dateOFBirth;
+        string gender;
+        string phoneNumber;
+        string email;
+
+        public:
+        Customer(string name, int customerId, string dateOFBirth, string gender, string phoneNumber, string email) {
+            this -> name = name;
+            this -> customerId = customerId;
+            this -> dateOFBirth = dateOFBirth;
+            this -> gender = gender;
+            this -> phoneNumber = phoneNumber;
+            this -> email = email;
+        }
+
+        string getname() {
+            return name;
+        }
+
+        void setname(string name) {
+            this -> name = name;
+        }
+        int getcustomerId() {
+            return customerId;
+        }
+
+        void setcustomerId (int customerId) {
+            this -> customerId = customerId;
+        }
+        string getdateOFBirth() {
+            return dateOFBirth;
+        }
+        void setdateOFbirth(string dateIFBirth) {
+            this -> dateOFBirth = dateOFBirth;
+        }
+        string getgender() {
+            return gender;
+        }
+        void setgender(string gender) {
+            this -> gender = gender;
+        }
+        string getphoneNumber() {
+            return phoneNumber;
+        }
+        void setphoneNumber(string phoneNumber) {
+            this -> phoneNumber = phoneNumber;
+        }
+        string email() {
+            return email;
+        }
+        void setemail(string email) {
+            this -> email = email;
+        }
+
+        void displayInfo() {
+            cout << "Name:" << name << endl;
+            cout << "Customer ID:" << customerId << endl;
+            cout << "Date of Birth:" << dateOFBirth << endl;
+            cout << "Gender:" << gender << endl;
+            cout << "Phone Number:" << phoneNumber << endl;
+            cout << "Email:" << email << endl;
+        }
+
+    };
 int main(){
     Car car1("Toyota", "Black", 101, 2022, 50, true, 4, "Petrol", true);
 
